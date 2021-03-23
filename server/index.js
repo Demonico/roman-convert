5e9fca980e73abf2c530a3734219f212b8815070
Jarrod <Demonico@users.noreply.github.com>
2021-03-22 21:38:19 -0600

    Basic server complete

--- server/index.js
+++ server/index.js
@@ -4,16 +4,24 @@ const conversions = require('./conversions')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get('/toRoman', (req, res) => {
+app.get('/',)
+
+app.get('/toRoman/:num', (req, res) => {
+  const {num} = req.params
   // check number meets constraints before passing value to function
-  const roman = conversions.toRomanNumeral()
-  res.send(roman)
+  const romanNumeral = conversions.toRomanNumeral(num)
+  res.send({romanNumeral})
 })
 
-app.get('/toArabic', (req, res) => {})
+app.get('/toArabic/:str', (req, res) => {
+  const {str} = req.params
+  const arabicNumeral = conversions.toArabicNumeral(str)
+  console.log(arabicNumeral)
+  res.send({arabicNumeral})
+})
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`)
